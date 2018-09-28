0375d9cb72ea67f6cccceb33780d007081155937
Max <38158634+maxdraz@users.noreply.github.com>
2018-09-28 12:12:07 +0100

    Added mouse drag scripts

---
+++ AnimaVenture Unity Project/Assets/mouseDrag.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseDrag : MonoBehaviour {
+
+    
+
+    private void OnMouseDrag()
+    {
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        Vector2 objPos = (Camera.main.ScreenToWorldPoint(mousePos));
+
+        transform.position = objPos;
+    }
+
+}
