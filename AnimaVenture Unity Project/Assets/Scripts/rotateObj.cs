fbfbdeff930c280378dd94834def32a6da0f831c
Connor Ryan <c16755215@mydit.ie>
2018-10-01 12:29:45 +0100

    Menu Spin
    Mneu spin added. No force atm. That's next step. Yay

--- AnimaVenture Unity Project/Assets/Scripts/rotateObj.cs
+++ AnimaVenture Unity Project/Assets/Scripts/rotateObj.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class rotateObj : MonoBehaviour {
 
-	
+	private float angle = 0.0f;
+    
+	void OnMouseDown()
+	{
+		Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+		pos = Input.mousePosition - pos;
+		angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+		angle -= Mathf.Atan2(transform.right.y, transform.right.x) * Mathf.Rad2Deg;
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        pos = Input.mousePosition - pos;
+		float ang = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - angle;
+		transform.rotation = Quaternion.AngleAxis(ang, Vector3.forward);
+	}
+
 }
