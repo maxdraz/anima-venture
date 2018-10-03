9b48f82dfebda07ceb0169024087970eeb125844
Max <38158634+maxdraz@users.noreply.github.com>
2018-10-03 20:25:54 +0100

    More work on the prototype

---
+++ AnimaVenture Unity Project/Assets/Scripts/Simon Game/buttonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonController : MonoBehaviour {
+
+    SpriteRenderer thisRenderer;
+	// Use this for initialization
+	void Start () {
+        thisRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        thisRenderer.color = new Color(
+            thisRenderer.color.r,
+            thisRenderer.color.g,
+            thisRenderer.color.b,
+            1f);
+    }
+
+    private void OnMouseUp()
+    {
+        thisRenderer.color = new Color(
+            thisRenderer.color.r,
+            thisRenderer.color.g,
+            thisRenderer.color.b,
+            0.5f);
+    }
+}
