bdbd6815a52da65b382f6c6ac21b4dffef8f4962
Max <38158634+maxdraz@users.noreply.github.com>
2018-10-09 20:35:52 +0100

    Redone Simon prototype (WIP)

---
+++ AnimaVenture Unity Project/Assets/Scripts/Simon Game/GameManager_Simon.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_Simon : MonoBehaviour {
+    [Header("Variables for Designer")]
+    [SerializeField] float startDelay = 2f;
+    [SerializeField] int maxSequenceLength;
+    
+    //An array to store telegraphs
+    [SerializeField] Telegraph_Simon[] telegraphs;
+
+    //create a list to store colour sequence
+    [SerializeField] List<int> colourSequence;
+
+    GameObject startButton;
+    GameObject restartButton;
+
+    private void Awake()
+    {
+        //initialize all references
+        startButton = GameObject.Find("StartButton");
+        restartButton = GameObject.Find("RestartButton");
+        restartButton.SetActive(false);
+
+        //colourSequence.Count = colourSequenceLength;
+    }
+
+    void Start () {
+        Debug.Log(gameObject.name + ": Welcome! Press 'Start' to play");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(colourSequence.Count > maxSequenceLength)
+        {
+            // Restart Game
+        }
+	}
+
+    public void StartGame()
+    {
+        //make start button disappear       
+        startButton.SetActive(false);
+        //make restart button appear        
+        restartButton.SetActive(true);
+        // invoke pick random colour method
+        Invoke("PickRandomColour", startDelay);
+        
+
+    }
+
+    void PickRandomColour()
+    {
+        int randomColourIndex = Random.Range(0, telegraphs.Length);
+        //display telegraph
+        telegraphs[randomColourIndex].DisplayTelegraph();
+        
+        colourSequence.Add(randomColourIndex);
+    }
+
+    public void RestartGame()
+    {
+        //delete previous colour sequence
+        colourSequence.Clear();
+        //call start game function
+        restartButton.SetActive(false);
+        startButton.SetActive(true);        
+    }
+}
---
+++ AnimaVenture Unity Project/Assets/Scripts/Simon Game/Telegraph_Simon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telegraph_Simon : MonoBehaviour {
+    [SerializeField] float lightUpTime = 2f;
+    //Sprite renderer reference (only for prototype)
+    SpriteRenderer sRenderer;
+
+	// Use this for initialization
+	void Start () {
+        sRenderer = gameObject.GetComponent<SpriteRenderer>();
+        //(Prototype) set alpha value to half
+        ResetTelegraph();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //method for what happens when time to telegraph colour
+    public void DisplayTelegraph()
+    {
+        //this will be changed later to fit our flying stars
+        sRenderer.color = new Color(sRenderer.color.r,
+            sRenderer.color.g,
+            sRenderer.color.b,
+            1.0f);
+
+        Invoke("ResetTelegraph", lightUpTime);
+    }
+
+    public void ResetTelegraph()
+    {
+        sRenderer.color = new Color(sRenderer.color.r,
+            sRenderer.color.g,
+            sRenderer.color.b,
+            0.5f);
+    }
+
+}
