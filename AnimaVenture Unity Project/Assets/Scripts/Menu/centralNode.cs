9749bae02ad9429d52a644646d822267c79ed695
Connor Ryan <c16755215@mydit.ie>
2018-10-16 22:49:47 +0100

    Some scripts for menu change
    Functionality for menu

---
+++ AnimaVenture Unity Project/Assets/Scripts/Menu/centralNode.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class centralNode : MonoBehaviour {
+	
+	public Camera main;
+
+	//public Camera journeyCamera;
+    
+	public GameObject node;
+    //public GameObject journeySelect;
+    public GameObject menu;
+	public GameObject background1;
+	public GameObject background2;
+	public GameObject back;
+
+	private void Start()
+	{
+		//journeyCamera.SetActive(false);
+		//journeySelect.SetActive(false);
+	}
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.A))
+        {
+            //journeySelect.SetActive(true);
+            background1.SetActive(true);
+            background2.SetActive(false);
+            menu.SetActive(false);
+            node.SetActive(false);
+			back.SetActive(true);
+        }
+	}
+
+	private void OnMouseDown()
+	{
+		//journeySelect.SetActive(true);
+		background1.SetActive(true);
+		background2.SetActive(false);
+		menu.SetActive(false);
+		node.SetActive(false);
+		back.SetActive(true);
+	}
+}
+
