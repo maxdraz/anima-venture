0a18b40871dc335c88101251d598cf7601939f8c
Max <38158634+maxdraz@users.noreply.github.com>
2018-10-18 14:13:45 +0100

    Audio Manager Fully Implemented

---
+++ AnimaVenture Unity Project/Assets/Scripts/Destroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public void DestroySelf(float destroyTime = 0)
+    {
+        //if destroy time has been specified
+        if(destroyTime != 0)
+        {
+            //destory after time
+            Destroy(this.gameObject, destroyTime);
+        }
+        else
+        {
+            //if not, destroy staight away
+            Destroy(this.gameObject);
+        }
+    }
+}
