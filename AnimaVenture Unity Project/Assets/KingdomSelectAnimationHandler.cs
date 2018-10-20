7d01e839d6dc3b55de6bf6aadec96dae9af3bdfb
NicolasStevenson <38294505+NicolasStevenson@users.noreply.github.com>
2018-10-20 02:29:49 +0100

    Kingdom to Journey Transition reworked with new assets
    User can go forward and back now too!

--- AnimaVenture Unity Project/Assets/KingdomSelectAnimationHandler.cs
+++ AnimaVenture Unity Project/Assets/KingdomSelectAnimationHandler.cs
@@ -5,42 +5,57 @@ using UnityEngine;
 public class KingdomSelectAnimationHandler : MonoBehaviour {
 	
 	public GameObject centralNode;
-	//public GameObject journeySelect;
-	public GameObject menu;
+	public GameObject kingdomMenu;
+	public GameObject kingdomLines;
+	public GameObject kingdomDots;
 	public GameObject backButton;
 	public GameObject simonStartButton;
-	public GameObject orangeNode;
+	public GameObject journeyLines;
+	public GameObject journey1;
+	public GameObject journey2;
 	public Animation journeyAnim;
 
 
 	public void LeaveKingdom () {
-		
-		menu.SetActive (false);
+		Debug.Log ("Leave kingdom");
+		kingdomMenu.SetActive (false);
+		kingdomDots.SetActive (false);
+		kingdomLines.SetActive (false);
 		centralNode.GetComponent<CircleCollider2D> ().enabled = false;
-		journeyAnim.GetClip ("JourneyFadeIn");
+		journeyAnim.clip = journeyAnim.GetClip ("JourneyFadeIn");	
 		journeyAnim.Play ();
 	}
 
 
 	public void ArriveAtKingdom () {
+		Debug.Log ("arrive at kingdom");
 
-		menu.SetActive (true);
+		kingdomMenu.SetActive (true);
+		kingdomDots.SetActive (true);
+		kingdomLines.SetActive (true);
 		centralNode.GetComponent<CircleCollider2D> ().enabled = true;
 	}
 
 	public void LeaveJourney () {
+		Debug.Log ("Leave journey");
+
 		backButton.SetActive (false);
 		simonStartButton.SetActive (false);
-		orangeNode.SetActive (false);
-		journeyAnim.GetClip ("JourneyFadeOut");
+		journey1.SetActive (false);
+		journey2.SetActive (false);
+		journeyLines.SetActive (false);
+		journeyAnim.clip = journeyAnim.GetClip ("JourneyFadeOut");
 		journeyAnim.Play ();
 	}
 
 	public void ArriveAtJourney () {
+		Debug.Log ("arrive at journey");
 
 		backButton.SetActive (true);
 		simonStartButton.SetActive (true);
-		orangeNode.SetActive (true);
+		journeyLines.SetActive (true);
+		journey1.SetActive (true);
+		journey2.SetActive (true);
 
 	}
 }
--- AnimaVenture Unity Project/Assets/Scripts/Menu/centralNode.cs
+++ AnimaVenture Unity Project/Assets/Scripts/Menu/centralNode.cs
@@ -6,51 +6,11 @@ public class centralNode : MonoBehaviour {
 	
 	public Camera main;
 
-	//public Camera journeyCamera;
-    
-	public GameObject node;
-    //public GameObject journeySelect;
-    public GameObject menu;
-	public GameObject background1;
-	public GameObject background2;
-	public GameObject back;
-	public GameObject simon;
-    public GameObject botNode;
-	public Camera camera;
-
-
-	private void Start()
-	{
-		//journeyCamera.SetActive(false);
-		//journeySelect.SetActive(false);
-	}
-
-	private void Update()
-	{
-		if (Input.GetKey(KeyCode.A))
-        {
-            //journeySelect.SetActive(true);
-            background1.SetActive(true);
-            background2.SetActive(false);
-            menu.SetActive(false);
-            node.SetActive(false);
-			back.SetActive(true);
-			simon.SetActive(true);
-
-        }
-	}
+   
 
 	private void OnMouseDown()
 	{
-		//journeySelect.SetActive(true);
-//		background1.SetActive(true);
-//		background2.SetActive(false);
-//		menu.SetActive(false);
-//		node.SetActive(false);
-//		back.SetActive(true);
-//		simon.SetActive(true);
-//        botNode.SetActive(true);
-		camera.GetComponent<CameraFovLerp> ().ToggleLerpBool ();
+		main.GetComponent<CameraFovLerp>(). ToggleLerpBool ();
 	}
 }
 
