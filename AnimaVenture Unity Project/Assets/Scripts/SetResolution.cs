b022dbddfa8ac69c813f39e9b635dd29ab7e6f6e
Max <38158634+maxdraz@users.noreply.github.com>
2018-10-20 22:33:54 +0100

    Added Set Resolution script and made it so game windom can't be resized

---
+++ AnimaVenture Unity Project/Assets/Scripts/SetResolution.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetResolution : MonoBehaviour {
+
+    [SerializeField] int resWidth, resHeight;
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(resWidth, resHeight, false);
+	}
+	
+	
+}
