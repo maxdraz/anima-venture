5db42b2dde7e916b79bc45f60ed4bb131ae38aee
Connor Ryan <c16755215@mydit.ie>
2018-10-20 14:08:36 +0100

    Scripts for menu
    Things are a bit spazzy still. Had no idea this would be such a pain in the hole.
    Nevertheless, the object will slerp back to original pos if in any other position. Also adds a force to the spin based on distance of swipe with reagrds to time.
    Currently only adds a a force to spin to the left. Will fix that later

--- AnimaVenture Unity Project/Assets/Scripts/Menu/rotateObj.cs
+++ AnimaVenture Unity Project/Assets/Scripts/Menu/rotateObj.cs
@@ -2,67 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotateObj : MonoBehaviour {
-	//Roatations
-	Rigidbody rb;
+public class rotateObj : MonoBehaviour
+{
+    //Roatations
+    Rigidbody rb;
     public float speed = 0.1f;
+    public float turnSpeed = 10f;
     float lerpSpeed = 0.01f;
     Vector3 axis;
 
-	private float angle = 0.0f;
+    private float angle = 0.0f;
 
-	Vector2 startPos;
-	Vector2 endPos;
-	float touchSpeed;
-	Vector3 Torque;
-	float timeStart;
-	float timeEnd;
+    Vector2 startPos;
+    Vector2 endPos;
+    float touchSpeed;
+    Vector3 Torque;
+    float timeStart;
+    float timeEnd;
 
-	void OnMouseOver()
-	{
-		//Object position relative to scrreen
-		Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-		//Distance gameobject needs to travel to the cursor(finger)
-		pos = Input.mousePosition - pos;
+    void OnMouseOver()
+    {
+        //Object position relative to scrreen
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        //Distance gameobject needs to travel to the cursor(finger)
+        pos = Input.mousePosition - pos;
         //Calculate angle relative to position of mouse and change to degrees
-		angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
-		angle -= Mathf.Atan2(transform.right.y, transform.right.x) * Mathf.Rad2Deg;
-	}
+        angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+        angle -= Mathf.Atan2(transform.right.y, transform.right.x) * Mathf.Rad2Deg;
+    }
 
-	void OnMouseDrag()
-	{
-		Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+    void OnMouseDrag()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
         pos = Input.mousePosition - pos;
 
-		float ang = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - angle;
-		transform.rotation = Quaternion.AngleAxis(ang, Vector3.forward);
-        
-	}
+        float ang = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - angle;
+        transform.rotation = Quaternion.AngleAxis(ang, Vector3.forward);
 
-	private void OnMouseDown()
-	{
-		startPos = Input.mousePosition;
-		timeStart = Time.time;
-	}
+    }
 
-	private void OnMouseUp()
-	{
-		timeEnd = Time.time;
-		endPos = Input.mousePosition;
-		Vector2 deltaPos = endPos - startPos;
-		touchSpeed = deltaPos.magnitude / (timeEnd - timeStart);
-
-		Torque = new Vector3(0, 0, touchSpeed * 1);
-
-		rb.AddTorque(Torque, ForceMode.Impulse);
-        
-
-	}
-
-
-	void Start()
+    private void OnMouseDown()
     {
-		rb = GetComponent<Rigidbody>();
+        startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        timeStart = Time.time;
+    }
+
+    private void OnMouseUp()
+    {
+        timeEnd = Time.time;
+        endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 deltaPos = endPos - startPos;
+        touchSpeed = deltaPos.magnitude / (timeEnd - timeStart);
+
+        Torque = new Vector3(0, 0, touchSpeed * 1);
+        rb.AddTorque(Torque, ForceMode.Impulse);
+
+        Debug.Log("X component is " + deltaPos.x);
+        Debug.Log("Y component is " + deltaPos.y);
+
+        /*if(deltaPos.x > 0)
+        {
+            Torque = new Vector3(0, 0, touchSpeed * -1);
+            rb.AddTorque(Torque, ForceMode.Impulse);
+        }*/
+    }
+
+    void RotateTo(float ang)
+    {
+        Quaternion newAng = Quaternion.identity;
+        newAng.eulerAngles = new Vector3(0, 0, ang);
+        transform.rotation = Quaternion.Slerp(transform.rotation, newAng, Time.time * turnSpeed / 30);
+    }
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
         //axis = Vector3.forward;
     }
 
@@ -70,6 +85,8 @@ public class rotateObj : MonoBehaviour {
     {
         //speed = Mathf.Lerp(lerpSpeed, speed, Time.deltaTime);
         //transform.Rotate(axis, lerpSpeed * Time.deltaTime * 360);
+
+        //RotateTo(0);
     }
 
 }
--- AnimaVenture Unity Project/Assets/Scripts/Menu/rotation.cs
+++ AnimaVenture Unity Project/Assets/Scripts/Menu/rotation.cs
@@ -2,46 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotation : MonoBehaviour {
-    
-	private float angle = 0.1f;
+public class rotation : MonoBehaviour
+{
 
-	public float turnSpeed = 10f;
-	float lerpSpeed = 0.01f;
+    private float angle = 0.1f;
+
+    public float turnSpeed = 10f;
+    float lerpSpeed = 0.01f;
     Vector3 axis;
 
-	public bool moving;
-	private float angle1;
+    private float angle1;
 
+    public bool Rotate;
 
-	// Use this for initialization
-	void Start()
-	{
-		angle1 = transform.eulerAngles.z;
-	}
-	// Update is called once per frame
-	void Update()
-	{
-		if (transform.rotation.z >= -30 || transform.rotation.z <= 30 && !moving)
+    Rigidbody rb;
+    Vector2 startPos;
+    Vector2 endPos;
+    float touchSpeed;
+    Vector3 Torque;
+    float timeStart;
+    float timeEnd;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        angle1 = transform.eulerAngles.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Rotate == false)
         {
             RotateTo(0);
-            Debug.Log("Rotating to 0");
+            Debug.Log("force is " + Torque);
         }
+    }
 
-		/*if (transform.rotation.z >= 30 || transform.rotation.z <= 90 && !moving)
-		{
-            RotateTo(60);
-            Debug.Log("Rotating to 60");
-        }*/
-
-		/*if (transform.rotation.z >= 30 && transform.rotation.z <= 90 && !moving)
-        {
-            RotateTo(60);
-            Debug.Log("Rotating to 60");
-        }*/
-	}
-
-	void OnMouseOver()
+    void OnMouseOver()
     {
         //Object position relative to scrreen
         Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
@@ -50,34 +49,49 @@ public class rotation : MonoBehaviour {
         //Calculate angle relative to position of mouse and change to degrees
         angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
         angle -= Mathf.Atan2(transform.right.y, transform.right.x) * Mathf.Rad2Deg;
-		//Debug.Log("Angle is " + angle);
+        //Debug.Log("Angle is " + angle);
     }
 
-	void OnMouseDrag()
+    void OnMouseDrag()
+    {
+        RotateObj();
+        Rotate = true;
+    }
+
+    void RotateObj()
     {
         Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
         pos = Input.mousePosition - pos;
 
-		float ang = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - angle;
+        float ang = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - angle;
         transform.rotation = Quaternion.AngleAxis(ang, Vector3.forward);
-		moving = true;
-		//Debug.Log("Angle is " + ang);
+//      Debug.Log("Angle is " + ang);
     }
 
-	void RotateTo(float ang)
+    void RotateTo(float ang)
     {
         Quaternion newAng = Quaternion.identity;
         newAng.eulerAngles = new Vector3(0, 0, ang);
         transform.rotation = Quaternion.Slerp(transform.rotation, newAng, Time.time * turnSpeed / 30);
+        
     }
 
-	void OnMouseDown()
-	{
-		
-	}
+    void OnMouseDown()
+    {
+        startPos = Input.mousePosition;
+        timeStart = Time.time;
+    }
 
-	void OnMouseUp()
-	{
-		moving = false;
-	}
+    void OnMouseUp()
+    {
+        timeEnd = Time.time;
+        endPos = Input.mousePosition;
+        Vector2 deltaPos = endPos - startPos;
+        touchSpeed = deltaPos.magnitude / (timeEnd - timeStart);
+
+        Torque = new Vector3(0, 0, touchSpeed * 1);
+        rb.AddTorque(Torque, ForceMode.Force);
+
+        Rotate = false;
+    }
 }
