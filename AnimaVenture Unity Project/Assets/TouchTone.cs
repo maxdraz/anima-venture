3bead7c2ba982c878d2cf750dca7b103d5fadfff
NicolasStevenson <38294505+NicolasStevenson@users.noreply.github.com>
2018-10-25 20:01:23 +0100

    Flower of Life Interactions added to journey select

---
+++ AnimaVenture Unity Project/Assets/TouchTone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTone : MonoBehaviour {
+	public AudioManager audio;
+	public int ToneToPlay;
+	public float pitch;
+
+	void OnMouseEnter(){
+
+				audio.PlayClip (ToneToPlay, .1f, false, pitch);
+			
+	}
+}
