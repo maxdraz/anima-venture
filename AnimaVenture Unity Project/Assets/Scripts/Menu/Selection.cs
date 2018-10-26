9e0e87de2d076ff10054c8b5c81098a52b50cd59
Connor Ryan <c16755215@mydit.ie>
2018-10-26 12:49:13 +0100

    Journey select Juice
    Added rotations and breathing effects to journey select.
    All values are public so if you'd like to edit you can

---
+++ AnimaVenture Unity Project/Assets/Scripts/Menu/Selection.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selection : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public GameObject menu;
+    Collider col;
+
+    public Vector3 oldRotation;
+    public Vector3 currRotation;
+    public float turnSpeed = 10f;
+    public float z;
+    public Vector3 angularVel;
+
+    public bool stop = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        oldRotation = menu.transform.rotation.eulerAngles;
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angularVel = rb.angularVelocity;
+        z = angularVel.z * Mathf.Rad2Deg;
+
+        currRotation = menu.transform.rotation.eulerAngles - oldRotation;
+        oldRotation = currRotation;
+
+        if (Input.GetKeyDown("space"))
+        {
+            RotateTo(0);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("Hit " + other.gameObject.name);
+
+        /*rb.angularDrag = rb.angularDrag + 0.1f;
+
+        if (other.gameObject.name == "Forest" && z < 1f && z > -1f)
+        {
+            //rb.angularVelocity = Vector3.zero;
+            Debug.Log("OI " + z);
+            RotateTo(0);
+        }*/
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //RotateTo(60);
+    }
+
+    void RotateTo(float ang)
+    {
+        Quaternion newAng = Quaternion.identity;
+        newAng.eulerAngles = new Vector3(0, 0, ang);
+        menu.transform.rotation = Quaternion.Slerp(menu.transform.rotation, newAng, Time.time * turnSpeed / 300);
+    }
+
+}
---
+++ AnimaVenture Unity Project/Assets/Scripts/Menu/TopJourneyRotation.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopJourneyRotation : MonoBehaviour {
+
+    Rigidbody rb;
+    Vector3 axis;
+    public float speed = 0.1f;
+    public float lerpSpeed = 0.01f;
+    public float scale;
+    public float maxSize;
+    public float growSize;
+    public float time;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        axis = Vector3.back;
+
+        StartCoroutine(Scale());
+    }
+
+    void Update()
+    {
+        speed = Mathf.Lerp(lerpSpeed, speed, Time.deltaTime);
+        transform.Rotate(axis, lerpSpeed * Time.deltaTime * 360);
+    }
+
+    IEnumerator Scale()
+    {
+        float timer = 0;
+
+        while (true)
+        {
+            while (maxSize > transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale += new Vector3(.2f, .2f, .2f) * Time.deltaTime * growSize;
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(time);
+
+            timer = 0;
+
+            while (scale < transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale -= new Vector3(.2f, .2f, .2f) * Time.deltaTime * growSize;
+                yield return null;
+
+            }
+
+            timer = 0;
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
