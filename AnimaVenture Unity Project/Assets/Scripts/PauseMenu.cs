41c03706c9eb6ab381809699b188a387cb9e21c7
Max <38158634+maxdraz@users.noreply.github.com>
2018-11-04 22:21:09 +0000

    Added Pause Menu

---
+++ AnimaVenture Unity Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pauseMenu;
+
+    public void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+       
+    }
+}
