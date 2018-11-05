d9e9c6747baa8103acc34c9cbaad560fee7c3b7f
Max <38158634+maxdraz@users.noreply.github.com>
2018-11-05 19:01:19 +0000

    Arrows fixed in Menu (push from class)

--- AnimaVenture Unity Project/Assets/Scenes/ArrowScript.cs
+++ AnimaVenture Unity Project/Assets/Scenes/ArrowScript.cs
@@ -4,66 +4,43 @@ using UnityEngine;
 
 public class ArrowScript : MonoBehaviour {
 	Transform mousePos;
-	float startXPos;
-	public float startYPos;
-	public float topLimit;
-	public float bottomLimit;
+    Vector3 startPos;
+	public float botomLimit;
 
-	public bool invertedGravity;
-	Vector3 inputPos;
 	bool mouseOver;
+    bool lerpBack = false;
 
 	// Use this for initialization
 	void Awake () {
 		mousePos = GameObject.Find ("MouseTransform").transform;
 		mouseOver = false;
-		startYPos = transform.position.y;
-		startXPos = transform.position.x;
+        startPos = transform.position;
 
-		if (invertedGravity) {
-			topLimit = startYPos;
-			bottomLimit = topLimit - .7f;
-
-		} else {
 		
-			bottomLimit = startYPos;
-			topLimit = bottomLimit + .7f;
-		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (mouseOver) {
-			
-//			inputPos = Input.mousePosition;
-//			inputPos = Camera.main.ScreenToWorldPoint(inputPos);
-//			transform.position = new Vector3 (transform.position.x, inputPos.y, 0);
-
-			Debug.Log ("over" + transform.position);
-
-		}
-
-//		if (transform.position.y >= topLimit) {
-//
-//			transform.position = new Vector3 (startXPos, topLimit, transform.position.z);
-//		}
-//
-//		if (transform.position.y <= bottomLimit) {
-//
-//			transform.position = new Vector3 (startXPos, bottomLimit, transform.position.z);
-//		}
-	}
+		
+        if (lerpBack)
+        {
+            transform.position = Vector3.Lerp(transform.position, startPos, 1f * Time.deltaTime);
+        }
+    }
 
 
 
 	void OnMouseEnter() {
 		transform.parent = mousePos;
 		mouseOver = true;
+        lerpBack = false;
 	}
 
 	void OnMouseExit () {
 		transform.parent = null;
 		mouseOver = false;
-		Debug.Log ("Gone" + transform.position);
+        lerpBack = true;
 	}
+
+   
 }
