2a911646ccc223a8101a593d1f026188531fa9ed
Max <38158634+maxdraz@users.noreply.github.com>
2018-11-06 14:49:05 +0000

    Updating Simon Telegraph animations

---
+++ AnimaVenture Unity Project/Assets/Scripts/Simon Game/Telegraph.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telegraph : MonoBehaviour {
+
+    [Range(0, 3)]
+    public int colourIndex;
+    public List<Sprite> symbolSprites;
+    public List<Color> glowColours;
+
+    SpriteRenderer sr;
+    public GameObject glow;
+    public ParticleSystem ps;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        
+    }
+
+    private void OnEnable()
+    {
+        sr.sprite = symbolSprites[colourIndex];
+        glow.GetComponent<SpriteRenderer>().color = glowColours[colourIndex];
+        var main = ps.main;
+        main.startColor = glowColours[colourIndex];
+    }
+}
