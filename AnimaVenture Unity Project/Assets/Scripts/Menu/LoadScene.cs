85fd68ed59ce2ec7360aeec73927f392cef11241
Max <38158634+maxdraz@users.noreply.github.com>
2018-11-12 11:50:25 +0000

    Added transition to meditation scene

--- AnimaVenture Unity Project/Assets/Scripts/Menu/LoadScene.cs
+++ AnimaVenture Unity Project/Assets/Scripts/Menu/LoadScene.cs
@@ -6,9 +6,25 @@ using UnityEngine.SceneManagement;
 public class LoadScene : MonoBehaviour {
 
     public int sceneIndex;
+    public bool loadWithTimerBool = false;
+    public float loadAfterTime = 2;
+
+    private void Start()
+    {
+        if (loadWithTimerBool)
+        {
+            StartCoroutine(LoadSceneAfterTime());
+        }
+    }
 
     public void LoadThisScene()
     {
+        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+    }
+
+    public IEnumerator LoadSceneAfterTime()
+    {
+        yield return new WaitForSeconds(loadAfterTime);
         SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
     }
 }
