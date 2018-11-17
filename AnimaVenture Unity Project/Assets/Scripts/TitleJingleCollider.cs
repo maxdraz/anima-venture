3c9bebe49565710449e91abd0d55165c3d0443ed
NicolasStevenson <38294505+NicolasStevenson@users.noreply.github.com>
2018-11-17 03:22:51 +0000

    Fixed transition to k-select

--- AnimaVenture Unity Project/Assets/Scripts/TitleJingleCollider.cs
+++ AnimaVenture Unity Project/Assets/Scripts/TitleJingleCollider.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class TitleJingleCollider : MonoBehaviour {
 
+	ParticleSystem particle;
 
-	
+
+	public void PlayParticle() {
+		particle = GetComponent<ParticleSystem> ();
+		particle.Play ();
+	}
+
+
 	void OnTriggerEnter () {
 		Debug.Log ("Triggered");
 		gameObject.GetComponent<AudioSource> ().Play();
