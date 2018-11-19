d8ca21614aa4d84e1820f983bcba55f1d5d3dec7
Max <38158634+maxdraz@users.noreply.github.com>
2018-11-19 12:21:22 +0000

    Made Danny's scene the new Menu select scene

---
+++ AnimaVenture Unity Project/Assets/Scripts/SetActiveAfterTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveAfterTime : MonoBehaviour {
+
+    public List<GameObject> buttons;
+    public AnimationClip introAnim;
+
+    private void Start()
+    {
+        StartCoroutine(SetAllActiveAfterTime(introAnim.length));
+    }
+
+    IEnumerator SetAllActiveAfterTime(float t) {
+        yield return new WaitForSeconds(t);
+
+        foreach(GameObject obj in buttons)
+        {
+            obj.SetActive(true);
+        }
+    }
+}
