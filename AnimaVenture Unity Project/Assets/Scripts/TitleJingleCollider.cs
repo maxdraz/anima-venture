d50d89d6be35dd15a76c240b94bd36a73435cf07
NicolasStevenson <38294505+NicolasStevenson@users.noreply.github.com>
2018-11-19 02:56:26 +0000

    Finalized particle for Title intro anim

--- AnimaVenture Unity Project/Assets/Scripts/TitleJingleCollider.cs
+++ AnimaVenture Unity Project/Assets/Scripts/TitleJingleCollider.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleJingleCollider : MonoBehaviour {
-
+	
+	Text letter;
 	ParticleSystem particle;
+
+
 
 
 	public void PlayParticle() {
@@ -12,6 +16,10 @@ public class TitleJingleCollider : MonoBehaviour {
 		particle.Play ();
 	}
 
+	public void ActivateLetter() {
+		letter = GetComponent<Text> ();
+		letter.enabled = true;
+	}
 
 	void OnTriggerEnter () {
 		Debug.Log ("Triggered");
