8ca4538df846cc5022bfe0a69f320bfd1c75c001
Connor Ryan <c16755215@mydit.ie>
2018-11-21 01:29:45 +0000

    Settings Sliders functional. Spin added to start

---
+++ AnimaVenture Unity Project/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeControl : MonoBehaviour
+{
+
+    public AudioMixer master;
+
+    public void SetSfxLvl(float sfxLvl)
+    {
+        master.SetFloat("sfxVol", sfxLvl);
+    }
+
+    public void SetMusicLvl(float musicLvl)
+    {
+        master.SetFloat("musicVol", musicLvl);
+    }
+
+    public void SetMasterLvl(float masterLvl)
+    {
+        master.SetFloat("masterVol", masterLvl);
+    }
+
+}
