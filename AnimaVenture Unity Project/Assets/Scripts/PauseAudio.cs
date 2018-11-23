3fd23c766cab2df76b8636fdbd2ca4a7b2c16d55
Max <38158634+maxdraz@users.noreply.github.com>
2018-11-23 13:26:03 +0000

    Added Pause button to meditation scene (still working on making it fade volume out)

---
+++ AnimaVenture Unity Project/Assets/Scripts/PauseAudio.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PauseAudio : MonoBehaviour {
+
+    public AudioSource audio;
+    public AudioMixer mixer;
+
+    public void PauseTrack()
+    {
+        
+        StartCoroutine(FadeOutVolume(80f, 0));
+        
+    }
+
+   public IEnumerator FadeOutVolume(float speed, float maxVolume)
+    {
+        while (true)
+        {           
+
+            maxVolume -= speed * Time.deltaTime;
+            Debug.Log(maxVolume);
+            mixer.SetFloat("meditationVol", maxVolume);
+
+            if (maxVolume <= -80)
+            {
+                audio.Pause();
+                this.gameObject.SetActive(false);
+                yield break;
+            }
+
+            yield return null;
+        }
+
+       
+    }
+    
+
+    }
+
