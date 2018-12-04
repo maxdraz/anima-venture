aa4329f3ffe0363d7b6f1e66c868cec899814854
Max <38158634+maxdraz@users.noreply.github.com>
2018-12-04 12:58:10 +0000

    Added Skip Animation script

---
+++ AnimaVenture Unity Project/Assets/Scripts/SkipAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipAnimation : MonoBehaviour {
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Skip();
+        }
+    }
+
+    void Skip()
+    {
+        if (animator != null)
+        {
+            animator.CrossFade("End State", 0f);
+        }
+    }
+}
