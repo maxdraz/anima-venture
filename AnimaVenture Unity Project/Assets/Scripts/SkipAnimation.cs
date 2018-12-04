9fa2c08c50a0344b6d6347784e1f286cfbe4f634
Max <38158634+maxdraz@users.noreply.github.com>
2018-12-04 19:17:21 +0000

    Added Skip Animation functionality

--- AnimaVenture Unity Project/Assets/Scripts/SkipAnimation.cs
+++ AnimaVenture Unity Project/Assets/Scripts/SkipAnimation.cs
@@ -2,18 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkipAnimation : MonoBehaviour {
+public class SkipAnimation : MonoBehaviour
+{
 
     Animator animator;
+    [SerializeField] bool tappedOnce;
+    [SerializeField] bool tappedTwice;
+    [SerializeField] float tapCooldown = 2f;
+    float originalCooldown;
+    public GameObject skipText;
 
-	// Use this for initialization
-	void Start () {
+
+
+    // Use this for initialization
+    void Start()
+    {
         animator = GetComponent<Animator>();
-	}
+        originalCooldown = tapCooldown;
+
+
+    }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && tappedOnce == false)
+        {
+            tappedOnce = true;
+            if (skipText != null)
+            {
+                skipText.SetActive(true);
+            }
+            StartCoroutine(StartCooldown());
+        }
+
+
+
+        if (tappedOnce && tappedTwice)
         {
             Skip();
         }
@@ -21,9 +45,50 @@ public class SkipAnimation : MonoBehaviour {
 
     void Skip()
     {
+        if (skipText != null)
+        {
+            skipText.SetActive(false);
+        }
         if (animator != null)
         {
             animator.CrossFade("End State", 0f);
+        }
+    }
+
+    IEnumerator StartCooldown()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.01f);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                tappedTwice = true;
+            }
+            tapCooldown -= Time.deltaTime;
+            if (tapCooldown <= 0)
+            {
+                tappedOnce = false;
+                tappedTwice = false;
+                tapCooldown = originalCooldown;
+                if (skipText != null)
+                {
+                    skipText.SetActive(false);
+                }
+                yield break;
+
+            }
+            else if (tappedOnce && tappedTwice)
+            {
+                yield return null;
+                tapCooldown = originalCooldown;
+                tappedOnce = false;
+                tappedTwice = false;
+                skipText.SetActive(false);
+                this.enabled = false;
+                yield break;
+            }
+            yield return null;
         }
     }
 }
