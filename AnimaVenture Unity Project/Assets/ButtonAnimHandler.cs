a39fd51abc8e5c820eabe9fe332605439cc69575
NicolasStevenson <38294505+NicolasStevenson@users.noreply.github.com>
2018-12-09 04:25:31 +0000

    Created UiButtonFadeIn/Out animations

---
+++ AnimaVenture Unity Project/Assets/ButtonAnimHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnimHandler : MonoBehaviour {
+
+	Animation anim;
+
+	// Use this for initialization
+	void Awake () {
+		anim = GetComponent<Animation> ();	
+		FadeIn();
+	}
+		
+	
+
+
+	public void FadeIn () {
+	
+		anim.clip = anim.GetClip ("Ui Button FadeIn");
+		anim.Play ();
+		Debug.Log ("FADED IN");
+	}
+
+	public void FadeOut () {
+
+		anim.clip = anim.GetClip ("Ui Button FadeOut");
+		anim.Play ();
+		Debug.Log ("FADED OUT");
+	}
+}
