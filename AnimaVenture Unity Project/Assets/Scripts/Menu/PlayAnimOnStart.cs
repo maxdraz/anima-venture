672946ea543a293982a4a72ddbf10d55571a3eee
Max <38158634+maxdraz@users.noreply.github.com>
2018-12-10 18:47:21 +0000

    Push from class

---
+++ AnimaVenture Unity Project/Assets/Scripts/Menu/PlayAnimOnStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimOnStart : MonoBehaviour {
+
+    public bool PlayOnStart;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        if (PlayOnStart) {
+            anim.SetBool("Wait", false);
+        }
+	}
+}
